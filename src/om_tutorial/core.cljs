2f90a32d11e07db0976373667c7c952969906d65
Paul Fernandez <paul4nandez@gmail.com>
2016-11-17 11:20:07 -0800

    Removed cruft from toolkit branch.

--- src/om_tutorial/core.cljs
+++ src/om_tutorial/core.cljs
@@ -1,20 +1,26 @@
 (ns om-tutorial.core
   (:require [goog.dom :as gdom]
             [om.next :as om :refer-macros [defui]]
-            [om.dom :as dom]
-            [om-tutorial.client :as t]))
+            [om.dom :as dom]))
 
-;;TODO: Remove om-tutorial.client from master. It only belongs on the toolkit branch.
+(defmulti read om/dispatch)
+(defmulti mutate om/dispatch)
+
+(defmethod read :default
+  [{:keys [state] :as env} key params]
+  (let [st @state]
+    (if-let [[_ value] (find st key)]
+      {:value value}
+      {:value :not-found})))
 
 (def app-state
   (atom
     {:app/title "Animals"
      :animals/list
      [[1 "Ant"] [2 "Antelope"] [3 "Bird"] [4 "Cat"] [5 "Dog"]
-      [6 "Lion"] [7 "Mouse"] [8 "Monkey"] [9 "Snake"] [10 "Zebra"]]
-     :counter 0}))
+      [6 "Lion"] [7 "Mouse"] [8 "Monkey"] [9 "Snake"] [10 "Zebra"]]}))
 
-(defmethod t/read :animals/list
+(defmethod read :animals/list
   [{:keys [state] :as env} key {:keys [start end]}]
   {:value (subvec (:animals/list @state) start end)})
 
@@ -24,8 +30,7 @@
     {:start 0 :end 10})
   static om/IQuery
   (query [this]
-    '[:app/title (:animals/list {:start ?start :end ?end})
-      :counter])
+    '[:app/title (:animals/list {:start ?start :end ?end})])
   Object
   (render [this]
     (let [{:keys [app/title animals/list counter]} (om/props this)]
@@ -35,14 +40,14 @@
           (map
             (fn [[i name]]
               (dom/li nil (str i ". " name)))
-            list))
-        (dom/h3 nil "Counter:")
-        (dom/div nil counter)))))
+            list))))))
 
 (def reconciler
   (om/reconciler
     {:state app-state
-     :parser t/parser}))
+     :parser (om/parser
+               {:read read
+                :mutate mutate})}))
 
 (om/add-root! reconciler
   AnimalsList (gdom/getElement "app"))
